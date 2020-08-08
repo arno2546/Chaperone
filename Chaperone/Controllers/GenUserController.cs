98d8090a80c4c6d9201dfd878af6674992198002
arno2546 <arno.ahmed2546@gmail.com>
2020-08-08 21:37:55 +0600

    Implemented Request
    working request

--- Chaperone/Controllers/GenUserController.cs
+++ Chaperone/Controllers/GenUserController.cs
@@ -9,7 +9,9 @@ namespace Chaperone.Controllers
 {
     public class GenUserController : Controller
     {
+        public List<User> Users;
         // GET: GenUser
+        [HttpGet]
         public ActionResult Index()
         {
             int i = (int)Session["LoggedIn"];
@@ -20,7 +22,115 @@ namespace Chaperone.Controllers
             }
             return RedirectToAction("Index", "Home");
         }
-
+        [HttpPost]
+        public ActionResult Index(SearchModel s)
+        {
+            if (ModelState.IsValid)
+            {
+                ChaperoneEntities che = new ChaperoneEntities();
+                List<User> tempUsers = che.Users.ToList();
+                Session["startDate"] = s.StartDate.ToString("dd/MM/yyyy");
+                Session["endDate"] = s.EndDate.ToString("dd/MM/yyyy");
+                Session["Location"] = s.searchString;
+                Users = new List<User>();
+                foreach (User u in tempUsers)
+                {
+                    if (u.User_type == "Guide")
+                    {
+                        if (u.Location.ToString().Contains(s.searchString.ToString()))
+                        {
+                            Users.Add(u);
+                        }
+                    }
+                }
+                tempUsers = Users.ToList();
+                if (s.Female)
+                {
+                    Users = new List<User>();
+                    foreach (User u in tempUsers.ToList())
+                    {
+                        if (u.Gender == "Female")
+                        {
+                            Users.Add(u);
+                        }
+                    }
+                }
+                tempUsers = Users.ToList();
+                if (s.Male)
+                {
+                    Users = new List<User>();
+                    foreach (User u in tempUsers.ToList())
+                    {
+                        if (u.Gender == "Male")
+                        {
+                            Users.Add(u);
+                        }
+                    }
+                }
+                tempUsers = Users.ToList();
+                if (s.culture)
+                {
+                    Users = new List<User>();
+                    foreach (User u in tempUsers.ToList())
+                    {
+                        if (Convert.ToBoolean(u.Culture))
+                        {
+                            Users.Add(u);
+                        }
+                    }
+                }
+                tempUsers = Users.ToList();
+                if (s.nightlife)
+                {
+                    Users = new List<User>();
+                    foreach (User u in tempUsers.ToList())
+                    {
+                        if (Convert.ToBoolean(u.NightLife))
+                        {
+                            Users.Add(u);
+                        }
+                    }
+                }
+                tempUsers = Users.ToList();
+                if (s.sports)
+                {
+                    Users = new List<User>();
+                    foreach (User u in tempUsers.ToList())
+                    {
+                        if (Convert.ToBoolean(u.Sports))
+                        {
+                            Users.Add(u);
+                        }
+                    }
+                }
+                tempUsers = Users.ToList();
+                if (s.festival)
+                {
+                    Users = new List<User>();
+                    foreach (User u in tempUsers.ToList())
+                    {
+                        if (Convert.ToBoolean(u.Festival))
+                        {
+                            Users.Add(u);
+                        }
+                    }
+                }
+                tempUsers = Users.ToList();
+                if (s.food)
+                {
+                    Users = new List<User>();
+                    foreach (User u in tempUsers.ToList())
+                    {
+                        if (Convert.ToBoolean(u.Food))
+                        {
+                            Users.Add(u);
+                        }
+                    }
+                }
+                return View(Users);
+            }
+            return View("~/Views/User/GenUserDash.cshtml", s);
+        }
         [HttpGet]
         public ActionResult Edit()
         {
@@ -56,7 +166,42 @@ namespace Chaperone.Controllers
             }
             return RedirectToAction("Index", "Home");
         }
+
+        [HttpGet]
+        public ActionResult GuideProfile(int id)
+        {
+            ChaperoneEntities che = new ChaperoneEntities();
+            User u = che.Users.Where(x => x.Id == id).FirstOrDefault();
+            return View(u);
+        }
+        [HttpPost]
+        [ActionName("GuideProfile")]
+        public ActionResult GuidedProfile(int id)
+        {
+            ChaperoneEntities che = new ChaperoneEntities();
+            int i = (int)Session["uid"];
+            User guide = che.Users.Where(x => x.Id == id).FirstOrDefault();
+            User tourist = che.Users.Where(x => x.Id == i).FirstOrDefault();
+            Request check = che.Requests.Where(x => x.TouristId == i && x.GuideId == id && x.RequestState=="Pending").FirstOrDefault();
+            if (check != null)
+            {
+                return RedirectToAction("Index");
+            }
+            Request r = new Request();
+            r.Location = (string)Session["Location"];
+            r.GuideId = guide.Id;
+            r.TouristId = tourist.Id;
+            r.StartDate = (string)Session["startDate"];
+            r.EndDate = (string)Session["endDate"];
+            r.RequestState = "Pending";
+            che.Requests.Add(r);
+            che.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
     }
+    
+    
 
 
 }
---
+++ Chaperone/Controllers/RequestController.cs
@@ -0,0 +1,33 @@
+﻿using Chaperone.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Chaperone.Controllers
+{
+    public class RequestController : Controller
+    {
+        // GET: Request
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult GenUserRequestIndex()
+        {
+            int i = (int)Session["LoggedIn"];
+            string u_type = (string)Session["u_type"];
+            int id = (int)Session["uid"];
+            if (i == 1 && u_type == "Gen")
+            {
+                ChaperoneEntities che = new ChaperoneEntities();
+                List<Request> userRequests = che.Requests.Where(x => x.TouristId == id).ToList();
+                return View(userRequests);
+            }
+            return RedirectToAction("Index");
+
+        }
+    }
+}
